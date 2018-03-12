4456d673b4781355cb6eee549d114c8517193756
Antoine Bellenoue <antoine.bellenoue@outlook.com>
2018-03-12 16:24:43 +0100

    insertion form

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -7,4 +7,46 @@
  */
 echo "<h1> inscription </h1>";
 
+if(isset($_POST['frmRegistration'])) {
+    $name = $_POST['name'] ?? "";
+    $firstName = $_POST['firstName'] ?? "";
+    $mail = $_POST['mail'] ?? "";
+    $password = $_POST['password'] ?? "";
+
+    $erreur = array();
+
+    if($name == "")
+        array_push($erreur,"veuiller saisir un nom");
+    if($firstName == "")
+        array_push($erreur,"veuiller saisir un prenom");
+    if($mail == "")
+        array_push($erreur,"veuiller saisir un mail");
+    if($password == "")
+        array_push($erreur,"veuiller saisir un mdp");
+
+    if(count($erreur) > 0){
+        $message = "<ul>";
+
+        for ($i = 0 ; $i < count($erreur) ; $i++){
+            $message .= "<li>";
+            $message .= $erreur[$i];
+            $message .= "</li>";
+        }
+
+        echo $message;
+        include "frmRegistration.php";
+
+        $message .="</ul>";
+    }
+
+    else{
+        //injection base
+        echo "merci devotre inscription ;)";
+    }
+
+}else{
+    include "frmRegistration.php";
+}
+
+
 ?>
